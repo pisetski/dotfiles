c41dfc45dd32d1f65c69869b34ffb6d7a10caf94
Pavel Pisetski <p.piseckiy@gmail.com>
2022-06-23 17:33:47 +0300

    Rm whitespace

--- .config/nvim/lua/pisetski/plugins.lua
+++ .config/nvim/lua/pisetski/plugins.lua
@@ -33,7 +33,7 @@ return require('packer').startup(function(use)
   use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   use 'neovim/nvim-lspconfig'
   use 'williamboman/nvim-lsp-installer'
-  use 'L3MON4D3/LuaSnip' 
+  use 'L3MON4D3/LuaSnip'
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-cmdline'
   use 'hrsh7th/cmp-nvim-lsp'
