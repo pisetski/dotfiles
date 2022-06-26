5fdd6cd357092903ea5972f6b68e8f1e34618501
Pavel Pisetski <p.piseckiy@gmail.com>
2022-06-26 16:28:23 +0300

    Setup null-ls formatting

---
+++ .config/nvim/lua/pisetski/plugin-null-ls.lua
@@ -0,0 +1,13 @@
+local lspconfig = require("lspconfig")
+local null_ls = require("null-ls")
+
+-- register any number of sources simultaneously
+local sources = {
+  null_ls.builtins.formatting.stylua,
+  null_ls.builtins.diagnostics.write_good,
+  null_ls.builtins.formatting.eslint_d.with({
+   prefer_local = "node_modules/.bin",
+  }),
+}
+
+null_ls.setup({ sources = sources, root_dir = lspconfig.util.root_pattern('package.json', 'yarn.lock', '.git'), })
--- .config/nvim/lua/pisetski/plugins.lua
+++ .config/nvim/lua/pisetski/plugins.lua
@@ -1,8 +1,8 @@
 -- Bootstrap the package manager -- no need to install anything separately like
 -- you have to do with vim-plug. Assumes that git is installed on the system
-local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
   vim.api.nvim_command 'packadd packer.nvim'
 end
 
@@ -10,6 +10,10 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim' -- Packer can manage itself
   use 'arcticicestudio/nord-vim'
   use 'tpope/vim-fugitive'
+  use { -- Unified highlight for all filetypes
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+  }
   use {
     'nvim-telescope/telescope.nvim',
     requires = {
@@ -30,7 +34,7 @@ return require('packer').startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons' }
   }
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   use 'neovim/nvim-lspconfig'
   use 'williamboman/nvim-lsp-installer'
   use 'L3MON4D3/LuaSnip'
@@ -38,8 +42,6 @@ return require('packer').startup(function(use)
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/nvim-cmp'
-  use 'pangloss/vim-javascript'
-  use 'leafgarland/typescript-vim' -- Typescript syntax highlighting
-  use 'maxmellon/vim-jsx-pretty' --  Make JSX look good (technically this supports TSX too, but has perf issues)
   use 'styled-components/vim-styled-components'
+  use 'jose-elias-alvarez/null-ls.nvim'
 end)
