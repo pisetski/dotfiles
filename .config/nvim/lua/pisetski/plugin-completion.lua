57dd00aca1ff3f707ecd29f5f9269bf6147efa93
Pavel Pisetski <p.piseckiy@gmail.com>
2022-06-26 13:43:41 +0300

    Rm tab functons

--- .config/nvim/lua/pisetski/plugin-completion.lua
+++ .config/nvim/lua/pisetski/plugin-completion.lua
@@ -20,10 +20,6 @@ cmp.setup({
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif vim.fn["vsnip#available"](1) == 1 then
-        feedkey("<Plug>(vsnip-expand-or-jump)", "")
-      elseif has_words_before() then
-        cmp.complete()
       else
         fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
       end
@@ -32,8 +28,6 @@ cmp.setup({
     ["<S-Tab>"] = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-        feedkey("<Plug>(vsnip-jump-prev)", "")
       end
     end, { "i", "s" }),
   },
