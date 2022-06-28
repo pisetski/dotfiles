c138e467ebc8518db08eb0fda00a0f9fa69e32b3
Pavel Pisetski <p.piseckiy@gmail.com>
2022-06-28 12:36:16 +0300

    Show relative file path

--- .config/nvim/lua/pisetski/plugin-lualine.lua
+++ .config/nvim/lua/pisetski/plugin-lualine.lua
@@ -7,12 +7,18 @@ require('lualine').setup {
     globalstatus = false,
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,
+        path = 1,
+      },
+    },
+    lualine_x = { 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
   },
   tabline = {},
   extensions = {}
