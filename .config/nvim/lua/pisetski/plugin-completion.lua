f20666f13c79fe262df4426bbdc36fc96a6f8086
pisetski <p.piseckiy@gmail.com>
2022-07-09 17:11:28 +0300

    Add completion signs

--- .config/nvim/lua/pisetski/plugin-completion.lua
+++ .config/nvim/lua/pisetski/plugin-completion.lua
@@ -2,6 +2,34 @@ local m = require('pisetski.mappings')
 local cmp = require('cmp')
 local luasnip = require('luasnip')
 
+local kind_icons = {
+  Text = "",
+  Method = "m",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "",
+  Interface = "",
+  Module = "",
+  Property = "",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
+
 cmp.setup({
   snippet = {
     -- REQUIRED - you must specify a snippet engine
@@ -37,11 +65,24 @@ cmp.setup({
       end
     end, { 'i', 's', 'c' }),
   },
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+      vim_item.menu = ({
+        nvim_lsp = "[LSP]",
+        luasnip = "[Snippet]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+      })[entry.source.name]
+
+      return vim_item
+    end,
+  },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
     { name = 'nvim_lsp_signature_help' },
-  }, {
     { name = 'buffer' },
   }),
   completion = { completeopt = 'menu,menuone,noinsert' },
