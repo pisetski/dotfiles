b039510052ed48300e2e82580e5d24a79a1b5e5e
pisetski <p.piseckiy@gmail.com>
2022-07-10 19:35:38 +0300

    Rm comment

--- .config/nvim/lua/pisetski/plugins.lua
+++ .config/nvim/lua/pisetski/plugins.lua
@@ -24,7 +24,7 @@ return require('packer').startup(function(use)
   use {
     'kyazdani42/nvim-tree.lua',
     requires = {
-      'kyazdani42/nvim-web-devicons', -- Optional, for file icons
+      'kyazdani42/nvim-web-devicons',
     },
   }
   use {
