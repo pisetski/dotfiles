54f22c05c446b6123f6aa4fc5406df48b1d11db9
Pavel Pisetski <p.piseckiy@gmail.com>
2022-07-12 18:34:57 +0300

    Rm gitsigns from null-ls suggestions

--- .config/nvim/lua/pisetski/null-ls.lua
+++ .config/nvim/lua/pisetski/null-ls.lua
@@ -7,7 +7,6 @@ local sources = {
   null_ls.builtins.formatting.eslint_d.with({
    prefer_local = 'node_modules/.bin',
   }),
-  null_ls.builtins.code_actions.gitsigns,
 }
 
 null_ls.setup({ sources = sources, root_dir = lspconfig.util.root_pattern('package.json', 'yarn.lock', '.git'), })
