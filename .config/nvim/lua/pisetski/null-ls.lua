7240a5fac6c9265e9ff26647e2f1b59a57213ca0
pisetski <p.piseckiy@gmail.com>
2022-07-27 14:43:13 +0300

    Add eslint_d null-ls sources

--- .config/nvim/lua/pisetski/null-ls.lua
+++ .config/nvim/lua/pisetski/null-ls.lua
@@ -3,10 +3,11 @@ local null_ls = require('null-ls')
 
 -- Register any number of sources simultaneously
 local sources = {
-  null_ls.builtins.diagnostics.write_good,
   null_ls.builtins.formatting.eslint_d.with({
-   prefer_local = 'node_modules/.bin',
+    prefer_local = 'node_modules/.bin',
   }),
+  null_ls.builtins.diagnostics.eslint_d,
+  null_ls.builtins.code_actions.eslint_d,
 }
 
 null_ls.setup({ sources = sources, root_dir = lspconfig.util.root_pattern('package.json', 'yarn.lock', '.git'), })
