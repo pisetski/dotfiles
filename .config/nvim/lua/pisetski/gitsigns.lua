f625631a21c00e5aacc73030d28304f04af794e5
Pavel Pisetski <p.piseckiy@gmail.com>
2022-09-01 22:52:08 +0200

    Change hunks navigation mapping

--- .config/nvim/lua/pisetski/gitsigns.lua
+++ .config/nvim/lua/pisetski/gitsigns.lua
@@ -48,14 +48,14 @@ require('gitsigns').setup {
     end
 
     -- Navigation
-    map('n', ']c', function()
-      if vim.wo.diff then return ']c' end
+    map('n', ']h', function()
+      if vim.wo.diff then return ']h' end
       vim.schedule(function() gs.next_hunk() end)
       return '<Ignore>'
     end, { expr = true })
 
-    map('n', '[c', function()
-      if vim.wo.diff then return '[c' end
+    map('n', '[h', function()
+      if vim.wo.diff then return '[h' end
       vim.schedule(function() gs.prev_hunk() end)
       return '<Ignore>'
     end, { expr = true })
