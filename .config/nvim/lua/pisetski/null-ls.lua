933eeff9758576e1dafe67d4f6df4cbd156a62df
Pavel Pisetski <p.piseckiy@gmail.com>
2022-09-11 00:57:25 +0300

    Rm eslint_d local preference

--- .config/nvim/lua/pisetski/null-ls.lua
+++ .config/nvim/lua/pisetski/null-ls.lua
@@ -3,13 +3,11 @@ local null_ls = require('null-ls')
 
 -- Register any number of sources simultaneously
 local sources = {
-  null_ls.builtins.formatting.eslint_d.with({
-    prefer_local = 'node_modules/.bin',
-  }),
+  null_ls.builtins.formatting.eslint_d,
   null_ls.builtins.diagnostics.eslint_d,
   null_ls.builtins.code_actions.eslint_d,
   null_ls.builtins.completion.spell,
   null_ls.builtins.completion.tags,
 }
 
-null_ls.setup({ sources = sources, root_dir = lspconfig.util.root_pattern('.git'), })
+null_ls.setup({ sources = sources, root_dir = lspconfig.util.root_pattern('.git') })
