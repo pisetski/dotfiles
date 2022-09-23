59cd9cb5671e9821ad560a899c339422f58f9df2
Pavel Pisetski <p.piseckiy@gmail.com>
2022-09-23 23:35:46 +0300

    Rm null-ls completion sources

--- .config/nvim/lua/pisetski/null-ls.lua
+++ .config/nvim/lua/pisetski/null-ls.lua
@@ -6,8 +6,6 @@ local sources = {
   null_ls.builtins.formatting.eslint_d,
   null_ls.builtins.diagnostics.eslint_d,
   null_ls.builtins.code_actions.eslint_d,
-  null_ls.builtins.completion.spell,
-  null_ls.builtins.completion.tags,
 }
 
 null_ls.setup({ sources = sources, root_dir = lspconfig.util.root_pattern('.git') })
