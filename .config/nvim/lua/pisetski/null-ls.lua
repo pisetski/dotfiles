1dbdb3cb614a36f017c6ec8ea3a9be26da32637e
Pavel Pisetski <p.piseckiy@gmail.com>
2022-09-24 12:54:13 +0300

    Add format on save

--- .config/nvim/lua/pisetski/null-ls.lua
+++ .config/nvim/lua/pisetski/null-ls.lua
@@ -1,11 +1,26 @@
 local lspconfig = require('lspconfig')
 local null_ls = require('null-ls')
 
--- Register any number of sources simultaneously
 local sources = {
   null_ls.builtins.formatting.eslint_d,
   null_ls.builtins.diagnostics.eslint_d,
   null_ls.builtins.code_actions.eslint_d,
 }
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-null_ls.setup({ sources = sources, root_dir = lspconfig.util.root_pattern('.git') })
+null_ls.setup({
+  sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.formatting_sync()
+        end,
+      })
+    end
+  end,
+  root_dir = lspconfig.util.root_pattern('.git')
+})
