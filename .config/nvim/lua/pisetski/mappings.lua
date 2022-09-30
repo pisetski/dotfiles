92143e624defee7c545bf86d0e9655c56d45f7e9
Pavel Pisetski <p.piseckiy@gmail.com>
2022-09-30 20:39:43 +0300

    Rm autoformat, add mapping

--- .config/nvim/lua/pisetski/mappings.lua
+++ .config/nvim/lua/pisetski/mappings.lua
@@ -115,6 +115,7 @@ function mappings:mapFzf()
   map('n', '<leader>fy', "<cmd>lua require('fzf-lua').registers()<CR>", silentnoremap)
   map('n', '<leader>fw', "<cmd>lua require('fzf-lua').grep_cword()<CR>", silentnoremap)
   map('n', '<leader>fW', "<cmd>lua require('fzf-lua').grep_cWORD()<CR>", silentnoremap)
+  map('n', '<leader>fs', "<cmd>lua require('fzf-lua').git_status()<CR>", silentnoremap)
 
   -- Pass custom cwd to grep
   map('n', '<leader>fd', ":FzfLua live_grep_native cwd=./", { noremap = true })
--- .config/nvim/lua/pisetski/null-ls.lua
+++ .config/nvim/lua/pisetski/null-ls.lua
@@ -6,23 +6,8 @@ local sources = {
   null_ls.builtins.diagnostics.eslint_d,
   null_ls.builtins.code_actions.eslint_d,
 }
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-local function format_on_save(client, bufnr)
-  if client.supports_method("textDocument/formatting") then
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = augroup,
-      buffer = bufnr,
-      callback = function()
-        vim.lsp.buf.formatting_sync()
-      end,
-    })
-  end
-end
 
 null_ls.setup({
   sources = sources,
-  on_attach = format_on_save,
   root_dir = lspconfig.util.root_pattern('.git')
 })
