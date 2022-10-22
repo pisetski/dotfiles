a07c9073616b27619fbcbb2b6c2b355aa71768bd
pisetski <p.piseckiy@gmail.com>
2022-10-22 22:16:44 +0300

    Change cmp sorting

--- .config/nvim/lua/pisetski/cmp.lua
+++ .config/nvim/lua/pisetski/cmp.lua
@@ -83,6 +83,17 @@ cmp.setup({
   completion = { completeopt = 'menu,menuone,noinsert' },
   performance = {
     debounce = 150
+  },
+  sorting = {
+    comparators = {
+      cmp.config.compare.score, -- based on :  score = score + ((#sources - (source_index - 1)) * sorting.priority_weight)
+      cmp.config.compare.offset,
+      -- compare.order,
+      -- compare.sort_text,
+      -- compare.exact,
+      -- compare.kind,
+      -- compare.length,
+    }
   }
 })
 
