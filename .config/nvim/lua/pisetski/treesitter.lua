f0cc97333fc4d8436becfc08006ac8b8b75400f7
Pavel Pisetski <p.piseckiy@gmail.com>
2022-12-03 11:23:34 +0300

    Add disabled incremental selection config to TS

--- .config/nvim/lua/pisetski/treesitter.lua
+++ .config/nvim/lua/pisetski/treesitter.lua
@@ -49,5 +49,15 @@ require 'nvim-treesitter.configs'.setup {
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
     -- colors = {}, -- table of hex strings
     -- termcolors = {} -- table of colour name strings
-  }
+  },
+
+  incremental_selection = {
+    enable = false,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
 }
