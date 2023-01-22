3343ac6d102eae4f20a9b6a5a09404dcfc8dcf28
Pavel Pisetski <p.piseckiy@gmail.com>
2023-01-22 15:15:49 +0300

    Use cwd detection func for eslint_d

--- .config/nvim/lua/pisetski/null-ls.lua
+++ .config/nvim/lua/pisetski/null-ls.lua
@@ -1,13 +1,29 @@
-local lspconfig = require('lspconfig')
 local null_ls = require('null-ls')
+local cwds = { "icc", "overlay", "online-checkin", "superadmin", "videocall" }
+
+local function findCWD(params)
+  for _, cwd in ipairs(cwds) do
+    if params.bufname:find(cwd) then
+      return cwd
+    end
+  end
+end
 
 local sources = {
-  null_ls.builtins.formatting.eslint_d,
-  null_ls.builtins.diagnostics.eslint_d,
-  null_ls.builtins.code_actions.eslint_d,
+  null_ls.builtins.formatting.eslint_d.with({
+    -- Falls back to bufname if return value is nil
+    cwd = findCWD,
+  }),
+  null_ls.builtins.diagnostics.eslint_d.with({
+    -- Falls back to bufname if return value is nil
+    cwd = findCWD,
+  }),
+  null_ls.builtins.code_actions.eslint_d.with({
+    -- Falls back to bufname if return value is nil
+    cwd = findCWD,
+  }),
 }
 
 null_ls.setup({
   sources = sources,
-  root_dir = lspconfig.util.root_pattern('.git')
 })
