7152314ffffc8439058e63a2925f2ed79805a2c6
Pavel Pisetski <p.piseckiy@gmail.com>
2023-01-22 15:19:07 +0300

    Auto install treesitter parsers

--- .config/nvim/lua/pisetski/treesitter.lua
+++ .config/nvim/lua/pisetski/treesitter.lua
@@ -6,6 +6,10 @@ require 'nvim-treesitter.configs'.setup {
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
   -- List of parsers to ignore installing (for 'all')
   ignore_install = {},
 
