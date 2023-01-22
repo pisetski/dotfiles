3db6f1ff07e62970d325189c34161e6a28e39a74
Pavel Pisetski <p.piseckiy@gmail.com>
2023-01-22 15:30:46 +0300

    Setup autopairs with coq

--- .config/nvim/lua/pisetski/autopairs.lua
+++ .config/nvim/lua/pisetski/autopairs.lua
@@ -1,15 +1,34 @@
-require("nvim-autopairs").setup({
+local npairs = require('nvim-autopairs')
+local remap = vim.api.nvim_set_keymap
+
+npairs.setup({
   check_ts = true,
   enable_check_bracket_line = true,
   disable_filetype = { "TelescopePrompt" },
-  fast_wrap = {
-    map = '<M-e>',
-    chars = { '{', '[', '(', '"', "'" },
-    pattern = [=[[%'%"%)%>%]%)%}%,]]=],
-    end_key = '$',
-    keys = 'qwertyuiopzxcvbnmasdfghjkl',
-    check_comma = true,
-    highlight = 'Search',
-    highlight_grey = 'Comment'
-  },
+  map_bs = false,
+  map_cr = false,
 })
+
+_G.MUtils = {}
+
+MUtils.CR = function()
+  if vim.fn.pumvisible() ~= 0 then
+    if vim.fn.complete_info({ 'selected' }).selected ~= -1 then
+      return npairs.esc('<c-y>')
+    else
+      return npairs.esc('<c-e>') .. npairs.autopairs_cr()
+    end
+  else
+    return npairs.autopairs_cr()
+  end
+end
+remap('i', '<cr>', 'v:lua.MUtils.CR()', { expr = true, noremap = true })
+
+MUtils.BS = function()
+  if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ 'mode' }).mode == 'eval' then
+    return npairs.esc('<c-e>') .. npairs.autopairs_bs()
+  else
+    return npairs.autopairs_bs()
+  end
+end
+remap('i', '<bs>', 'v:lua.MUtils.BS()', { expr = true, noremap = true })
