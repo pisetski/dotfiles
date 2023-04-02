38ffb9b0ee0b26606744d83395fee3e41b867297
Pavel Pisetski <p.piseckiy@gmail.com>
2023-04-02 14:31:00 +0300

    Use nvim-tree on_attach fn

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -7,55 +7,67 @@ return {
     "NvimTreeFocus"
   },
   keys = {
-    { '<leader>E', '<cmd>NvimTreeFindFile<cr>', desc = "NvimTreeFindFile" },
+    { '<leader>E', '<cmd>NvimTreeFindFile<cr>',       desc = "NvimTreeFindFile" },
     { '<leader>e', '<cmd>NvimTreeFindFileToggle<cr>', desc = "NvimTreeFindFileToggle" },
   },
-  opts = {
-    sort_by = 'case_sensitive',
-    open_on_setup = false,
-    view = {
-      side = "right",
-      adaptive_size = true,
-      centralize_selection = true,
-      mappings = {
-        list = {
-          { key = 's', action = '' },
-          { key = 'S', action = '' },
-        },
-      },
-    },
-    renderer = {
-      group_empty = true,
-      highlight_git = true,
-      indent_markers = {
-        enable = true,
-        inline_arrows = true,
-        icons = {
-          corner = "└",
-          edge = "│",
-          item = "│",
-          bottom = "─",
-          none = " ",
-        },
-      },
-    },
-    filters = {
-      dotfiles = false,
-      custom = {
-        '^\\.git'
-      }
-    },
-    actions = {
-      open_file = {
-        quit_on_open = true,
-      },
-    },
-    filesystem_watchers = {
-      enable = true,
-    },
-    diagnostics = {
-      enable = false
-    },
-  }
-}
+  config = function()
+    local function on_attach(bufnr)
+      local api = require('nvim-tree.api')
 
+      local function opts(desc)
+        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      api.config.mappings.default_on_attach(bufnr)
+
+      vim.keymap.del('n', 's', { buffer = bufnr })
+      vim.keymap.del('n', 'S', { buffer = bufnr })
+
+      vim.keymap.set('n', 'A', api.tree.expand_all, opts('Expand All'))
+      vim.keymap.set('n', 'Z', api.node.run.system, opts('Run System'))
+    end
+
+    require("nvim-tree").setup({
+      sort_by = 'case_sensitive',
+      open_on_setup = false,
+      view = {
+        side = "right",
+        adaptive_size = true,
+        centralize_selection = true,
+      },
+      on_attach = on_attach,
+      renderer = {
+        group_empty = true,
+        highlight_git = true,
+        indent_markers = {
+          enable = true,
+          inline_arrows = true,
+          icons = {
+            corner = "└",
+            edge = "│",
+            item = "│",
+            bottom = "─",
+            none = " ",
+          },
+        },
+      },
+      filters = {
+        dotfiles = false,
+        custom = {
+          '^\\.git'
+        }
+      },
+      actions = {
+        open_file = {
+          quit_on_open = true,
+        },
+      },
+      filesystem_watchers = {
+        enable = true,
+      },
+      diagnostics = {
+        enable = false
+      },
+    })
+  end
+}
