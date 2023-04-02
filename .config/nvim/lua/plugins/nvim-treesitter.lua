b2935c6845cf2a5be901858ca080ac6d4ad1dcf7
Pavel Pisetski <p.piseckiy@gmail.com>
2023-04-02 09:41:28 +0300

    Fix TS startup

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,64 +1,66 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = {
-    -- A list of parser names, or 'all'
-    ensure_installed = { 'css', 'lua', 'dockerfile', 'dot', 'html', 'javascript', 'json', 'make', 'markdown', 'regex',
-      'tsx', 'typescript', 'yaml', 'gitignore', 'bash', 'go', 'php' },
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,
-    -- List of parsers to ignore installing (for 'all')
-    ignore_install = {},
-    context_commentstring = {
-      enable = true,
-      enable_autocmd = false,
-    },
-    highlight = {
-      -- `false` will disable the whole extension
-      enable = true,
-      -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-      -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-      -- the name of the parser)
-      -- list of language that will be disabled
-      disable = {},
-      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-      -- Using this option may slow down your editor, and you may see some duplicate highlights.
-      -- Instead of true it can also be a list of languages
-      additional_vim_regex_highlighting = false,
-    },
-    indent = {
-      enable = true
-    },
-    autotag = {
-      enable = true,
-    },
-    refactor = {
-      highlight_definitions = {
+  config = function()
+    require('nvim-treesitter.configs').setup({
+      -- A list of parser names, or 'all'
+      ensure_installed = { 'css', 'lua', 'dockerfile', 'dot', 'html', 'javascript', 'json', 'make', 'markdown', 'regex',
+        'tsx', 'typescript', 'yaml', 'gitignore', 'bash', 'go', 'php' },
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+      auto_install = true,
+      -- List of parsers to ignore installing (for 'all')
+      ignore_install = {},
+      context_commentstring = {
         enable = true,
-        -- Set to false if you have an `updatetime` of ~100.
-        clear_on_cursor_move = true,
+        enable_autocmd = false,
       },
-    },
-    rainbow = {
-      enable = true,
-      -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-      extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-      max_file_lines = nil, -- Do not enable for files with more than n lines, int
-      -- colors = {}, -- table of hex strings
-      -- termcolors = {} -- table of colour name strings
-    },
-    incremental_selection = {
-      enable = false,
-      keymaps = {
-        init_selection = "gnn",
-        node_incremental = "grn",
-        scope_incremental = "grc",
-        node_decremental = "grm",
+      highlight = {
+        -- `false` will disable the whole extension
+        enable = true,
+        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+        -- the name of the parser)
+        -- list of language that will be disabled
+        disable = {},
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
       },
-    },
-  }
+      indent = {
+        enable = true
+      },
+      autotag = {
+        enable = true,
+      },
+      refactor = {
+        highlight_definitions = {
+          enable = true,
+          -- Set to false if you have an `updatetime` of ~100.
+          clear_on_cursor_move = true,
+        },
+      },
+      rainbow = {
+        enable = true,
+        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+        extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+        max_file_lines = nil, -- Do not enable for files with more than n lines, int
+        -- colors = {}, -- table of hex strings
+        -- termcolors = {} -- table of colour name strings
+      },
+      incremental_selection = {
+        enable = false,
+        keymaps = {
+          init_selection = "gnn",
+          node_incremental = "grn",
+          scope_incremental = "grc",
+          node_decremental = "grm",
+        },
+      },
+    })
+  end
 }
