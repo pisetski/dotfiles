b0f74bac6a3648634a4b17fb108228c7ae9690f4
Pavel Pisetski <p.piseckiy@gmail.com>
2023-04-02 10:25:48 +0300

    Refactor indent-blankline usage

---
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,7 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  config = {
+    show_current_context = false,
+    enabled = true,
+  }
+}
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -10,7 +10,6 @@ return {
     dependencies = { "kyazdani42/nvim-web-devicons" }
   },
   { "folke/tokyonight.nvim" },
-  { "lukas-reineke/indent-blankline.nvim" },
   { "kevinhwang91/nvim-hlslens" },
 
   -- Git
