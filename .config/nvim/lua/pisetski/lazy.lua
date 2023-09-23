4c5c3321c21e31510fcc308b0c69044f042cb366
Pavel Pisetski <p.piseckiy@gmail.com>
2023-09-23 12:32:01 +0200

    Disable lazy change detection

--- .config/nvim/lua/pisetski/lazy.lua
+++ .config/nvim/lua/pisetski/lazy.lua
@@ -11,4 +11,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup({
+  import = "plugins",
+  change_detection = {
+    notify = false,
+  },
+})
