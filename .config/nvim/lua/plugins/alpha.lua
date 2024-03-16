e78ffe401133ccce733bcd7aad165fe806da624a
Pavel Pisetski <pisetski@1234s-mbp.home>
2024-03-16 23:23:09 +0100

    Upd alpha icons

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -45,10 +45,10 @@ return {
     dashboard.section.buttons.val = {
       dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
       dashboard.button("p", "  Find file", "<CR>:FzfLua files<CR>"),
-      dashboard.button("e", "  Explore", ":ene<CR>:NvimTreeFocus<CR>"),
+      dashboard.button("e", "  Explore", ":ene<CR>:NvimTreeFocus<CR>"),
       dashboard.button("r", "  Recent files", ":FzfLua oldfiles<CR>"),
       dashboard.button("u", "  Update plugins and parsers", ":Lazy sync<CR> :TSUpdate<CR>"),
-      dashboard.button("q", "  Quit", ":qa<CR>"),
+      dashboard.button("q", "󰅘 Quit", ":qa<CR>"),
     }
 
     dashboard.config.opts.noautocmd = true
