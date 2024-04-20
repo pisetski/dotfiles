8faed246e268ff18fcb35081a41bb5516dfd4231
Pavel Pisetski <p.piseckiy@gmail.com>
2024-04-20 20:29:09 +0200

    Add nvim-tree function to move/copy via path

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -16,6 +16,50 @@ return {
     local function on_attach(bufnr)
       local api = require('nvim-tree.api')
 
+      local function copy_or_move_file_to(node, copy)
+        local file_src = node.absolute_path
+        -- The args of input are {prompt}, {default}, {completion}
+        -- Read in the new file path using the existing file's path as the baseline.
+        local cmd_str = copy and "COPY" or "MOVE"
+        local file_out = vim.fn.input(cmd_str .. " TO: ", file_src, "file")
+        -- Create any parent dirs as required
+        local dir = vim.fn.fnamemodify(file_out, ":h")
+
+        -- gitbash.exe has win32 but no move. A better check is if the system has the `mv` command
+        -- Note: In windows copy is also `cp`
+        local is_win = vim.fn.executable('mv') == 0
+
+        -- Make parent dir as required
+        local mkdir_cmd = is_win and { 'mkdir', dir } or { 'mkdir', '-p', dir }
+        vim.fn.system(mkdir_cmd)
+        local cmds = {
+          win = {
+            copy = { 'cp', file_src, file_out },
+            move = { 'move', file_src, file_out },
+          },
+          linux = {
+            copy = { 'cp', '-R', file_src, file_out },
+            move = { 'mv', file_src, file_out },
+          }
+        }
+        local cmd_arr = cmds[is_win and 'win' or 'linux'][copy and 'copy' or 'move']
+        vim.fn.system(cmd_arr)
+        if not copy then
+          -- If a move, then associate any open buffers with the moved file with the new file path
+          for _, buf in ipairs(vim.fn.getbufinfo()) do
+            if buf.name == file_src then
+              -- Set the buffer's name to the new destination path
+              print(buf.name, '\n', file_src, '\n', file_out)
+              vim.api.nvim_buf_set_name(buf.bufnr, file_out)
+              vim.api.nvim_buf_call(buf.bufnr, function()
+                vim.cmd('e')
+              end)
+            end
+          end
+          vim.cmd('redraw!')
+          vim.cmd('redrawstatus!') -- Doesn't work with my status line
+        end
+      end
       local function opts(desc)
         return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
       end
@@ -31,6 +75,19 @@ return {
 
       vim.keymap.set('n', 'A', api.tree.expand_all, opts('Expand All'))
       vim.keymap.set('n', 'Z', api.node.run.system, opts('Run System'))
+
+      vim.keymap.set('n', 'c', function()
+        local node = api.tree.get_node_under_cursor()
+        copy_or_move_file_to(node, true)
+      end, opts('copy_file_to'))
+
+      vim.keymap.set('n', 'm', function()
+        local node = api.tree.get_node_under_cursor()
+        -- If the buffer is open, move the buffer path too
+        local source_path = node.absolute_path
+        -- Iterate through all open buffers
+        copy_or_move_file_to(node, false)
+      end, opts('move_file_to'))
     end
 
     require("nvim-tree").setup({
