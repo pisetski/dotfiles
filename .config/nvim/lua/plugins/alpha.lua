2156ab6940ff1d99987747a8c5f3d34dd6e0451f
Pavel Pisetski <p.piseckiy@gmail.com>
2024-04-22 00:01:17 +0200

    Upd alpha

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -6,40 +6,14 @@ return {
     local dashboard = require('alpha.themes.dashboard')
 
     dashboard.section.header.val = {
-      "            :h-                                  Nhy`               ",
-      "           -mh.                           h.    `Ndho               ",
-      "           hmh+                          oNm.   oNdhh               ",
-      "          `Nmhd`                        /NNmd  /NNhhd               ",
-      "          -NNhhy                      `hMNmmm`+NNdhhh               ",
-      "          .NNmhhs              ```....`..-:/./mNdhhh+               ",
-      "           mNNdhhh-     `.-::///+++////++//:--.`-/sd`               ",
-      "           oNNNdhhdo..://++//++++++/+++//++///++/-.`                ",
-      "      y.   `mNNNmhhhdy+/++++//+/////++//+++///++////-` `/oos:       ",
-      " .    Nmy:  :NNNNmhhhhdy+/++/+++///:.....--:////+++///:.`:s+        ",
-      " h-   dNmNmy oNNNNNdhhhhy:/+/+++/-         ---:/+++//++//.`         ",
-      " hd+` -NNNy`./dNNNNNhhhh+-://///    -+oo:`  ::-:+////++///:`        ",
-      " /Nmhs+oss-:++/dNNNmhho:--::///    /mmmmmo  ../-///++///////.       ",
-      "  oNNdhhhhhhhs//osso/:---:::///    /yyyyso  ..o+-//////////:/.      ",
-      "   /mNNNmdhhhh/://+///::://////     -:::- ..+sy+:////////::/:/.     ",
-      "     /hNNNdhhs--:/+++////++/////.      ..-/yhhs-/////////::/::/`    ",
-      "       .ooo+/-::::/+///////++++//-/ossyyhhhhs/:///////:::/::::/:    ",
-      "       -///:::::::////++///+++/////:/+ooo+/::///////.::://::---+`   ",
-      "       /////+//++++/////+////-..//////////::-:::--`.:///:---:::/:   ",
-      "       //+++//++++++////+++///::--                 .::::-------::   ",
-      "       :/++++///////////++++//////.                -:/:----::../-   ",
-      "       -/++++//++///+//////////////               .::::---:::-.+`   ",
-      "       `////////////////////////////:.            --::-----...-/    ",
-      "        -///://////////////////////::::-..      :-:-:-..-::.`.+`    ",
-      "         :/://///:///::://::://::::::/:::::::-:---::-.-....``/- -   ",
-      "           ::::://::://::::::::::::::----------..-:....`.../- -+oo/ ",
-      "            -/:::-:::::---://:-::-::::----::---.-.......`-/.      ``",
-      "           s-`::--:::------:////----:---.-:::...-.....`./:          ",
-      "          yMNy.`::-.--::..-dmmhhhs-..-.-.......`.....-/:`           ",
-      "         oMNNNh. `-::--...:NNNdhhh/.--.`..``.......:/-              ",
-      "        :dy+:`      .-::-..NNNhhd+``..`...````.-::-`                ",
-      "                        .-:mNdhh:.......--::::-`                    ",
-      "                           yNh/..------..`                          ",
-      "                                                                    ",
+      [[                                                    ]],
+      [[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
+      [[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
+      [[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
+      [[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
+      [[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
+      [[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
+      [[                                                    ]],
     }
 
     dashboard.section.buttons.val = {
@@ -48,8 +22,36 @@ return {
       dashboard.button("e", "  Explore", ":ene<CR>:NvimTreeFocus<CR>"),
       dashboard.button("r", "  Recent files", ":FzfLua oldfiles<CR>"),
       dashboard.button("u", "  Update plugins and parsers", ":Lazy sync<CR> :TSUpdate<CR>"),
-      dashboard.button("q", "󰅘 Quit", ":qa<CR>"),
+      dashboard.button("q", "󰅘  Quit", ":qa<CR>"),
     }
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "LazyVimStarted",
+      callback = function()
+        local v = vim.version()
+        local dev = ""
+        if v.prerelease == "dev" then
+          dev = "-dev+" .. v.build
+        else
+          dev = ""
+        end
+        local version = v.major .. "." .. v.minor .. "." .. v.patch .. dev
+        local stats = require("lazy").stats()
+        local plugins_count = stats.loaded .. "/" .. stats.count
+        local ms = math.floor(stats.startuptime + 0.5)
+        local line1 = " " .. plugins_count .. " plugins loaded in " .. ms .. "ms"
+        local line3 = "v" .. version
+
+        local line1_width = vim.fn.strdisplaywidth(line1)
+        local line3Padded = string.rep(" ", (line1_width - vim.fn.strdisplaywidth(line3)) / 2) .. line3
+
+        dashboard.section.footer.val = {
+          line1,
+          line3Padded,
+        }
+        pcall(vim.cmd.AlphaRedraw)
+      end,
+    })
+
 
     dashboard.config.opts.noautocmd = true
     alpha.setup(dashboard.config)
