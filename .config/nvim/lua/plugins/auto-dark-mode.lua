47e307b1a13d68edb25c0cd60d71f728915726ca
Pavel Pisetski <p.piseckiy@gmail.com>
2024-05-18 23:10:33 +0200

    Rewrite deprecated usages

--- .config/nvim/lua/plugins/auto-dark-mode.lua
+++ .config/nvim/lua/plugins/auto-dark-mode.lua
@@ -3,12 +3,12 @@ return {
   opts = {
     update_interval = 1000,
     set_dark_mode = function()
-      vim.api.nvim_set_option("background", "dark")
+      vim.api.nvim_set_option_value("background", "dark", {})
       vim.cmd("colorscheme tokyonight")
       vim.env.BAT_THEME = "tokyonight_storm"
     end,
     set_light_mode = function()
-      vim.api.nvim_set_option("background", "light")
+      vim.api.nvim_set_option_value("background", "light", {})
       vim.cmd("colorscheme tokyonight-day")
       vim.env.BAT_THEME = "tokyonight_day"
     end,
