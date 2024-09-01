9e8ac4dca14d4e36eec6d641ff1f9fdbd5ea374e
Pavel Pisetski <p.piseckiy@gmail.com>
2024-09-01 17:10:11 +0200

    Install codecompanion

---
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,31 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("codecompanion").setup({
+      strategies = {
+        chat = {
+          adapter = "anthropic",
+        },
+        inline = {
+          adapter = "anthropic",
+        },
+        agent = {
+          adapter = "anthropic",
+        },
+      },
+      adapters = {
+        anthropic = function()
+          return require("codecompanion.adapters").extend("anthropic", {
+            env = {
+              api_key = "cmd:op read op://Personal/Anthropic/credential --no-newline"
+            },
+          })
+        end,
+      },
+    })
+  end
+}
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,6 +2,43 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "kyazdani42/nvim-web-devicons" },
   config = function()
+    local function create_codecompanion_component()
+      local M = require("lualine.component"):extend()
+      M.processing = false
+      M.spinner_index = 1
+      local spinner_symbols = {
+        "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏",
+      }
+      local spinner_symbols_len = 10
+
+      function M:init(options)
+        M.super.init(self, options)
+        local group = vim.api.nvim_create_augroup("CodeCompanionHooks", {})
+        vim.api.nvim_create_autocmd({ "User" }, {
+          pattern = "CodeCompanionRequest*",
+          group = group,
+          callback = function(request)
+            if request.match == "CodeCompanionRequestStarted" then
+              self.processing = true
+            elseif request.match == "CodeCompanionRequestFinished" then
+              self.processing = false
+            end
+          end,
+        })
+      end
+
+      function M:update_status()
+        if self.processing then
+          self.spinner_index = (self.spinner_index % spinner_symbols_len) + 1
+          return spinner_symbols[self.spinner_index]
+        else
+          return nil
+        end
+      end
+
+      return M
+    end
+
     require('lualine').setup({
       options = {
         icons_enabled = true,
@@ -37,6 +74,7 @@ return {
           },
         },
         lualine_x = {
+          create_codecompanion_component(),
           {
             'diagnostics',
             sources = { 'nvim_workspace_diagnostic' },
