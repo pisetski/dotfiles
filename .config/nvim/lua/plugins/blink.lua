6cac74af3a68bd21024f5b8a0aede00b0f84ff93
Pavel Pisetski <p.piseckiy@gmail.com>
2024-12-11 10:59:20 +0100

    Upd blink cfg

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -16,31 +16,34 @@ return {
   opts = {
     -- 'default' for mappings similar to built-in completion
     -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- see the "default configuration" section below for full documentation on how to define
-    -- your own keymap. when defining your own, no keybinds will be assigned automatically.
-    keymap = {
-      preset = "super-tab",
-    },
+    -- your own keymap.
+    keymap = { preset = 'super-tab' },
 
     appearance = {
-      nerd_font_variant = 'mono',
+      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- Useful for when your theme doesn't support blink.cmp
+      -- will be removed in a future release
+      use_nvim_cmp_as_default = true,
+      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'mono'
     },
-    -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-    -- adjusts spacing to ensure icons are aligned
 
-    signature = { enabled = true },
-
+    -- default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, via `opts_extend`
     sources = {
-      completion = {
-        enabled_providers = { 'lsp', 'path', 'snippets', 'buffer' },
-      },
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
 
-    completion = {
-      draw = {
-        columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 1 } },
-      },
-    }
+    -- experimental auto-brackets support
+    -- completion = { accept = { auto_brackets = { enabled = true } } }
+
+    -- experimental signature help support
+    signature = { enabled = true }
   },
-  opts_extend = { "sources.completion.enabled_providers" },
+  -- allows extending the providers array elsewhere in your config
+  -- without having to redefine it
+  opts_extend = { "sources.default" }
 }
