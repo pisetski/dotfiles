3e3b6ec3707d2e90fd43572ba4c932405e86f923
Pavel Pisetski <p.pisetski@gmail.com>
2025-04-20 21:51:49 +0200

    Upd blink to v1

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -5,7 +5,7 @@ return {
   dependencies = 'rafamadriz/friendly-snippets',
 
   -- use a release tag to download pre-built binaries
-  version = 'v0.*',
+  version = 'v1.*',
   -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
   -- build = 'cargo build --release',
   -- If you use nix, you can build from source using latest nightly rust with:
@@ -16,39 +16,39 @@ return {
   opts = {
     -- 'default' for mappings similar to built-in completion
     -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- see the "default configuration" section below for full documentation on how to define
-    -- your own keymap.
-    keymap = { preset = 'super-tab' },
-
-    appearance = {
-      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- Useful for when your theme doesn't support blink.cmp
-      -- will be removed in a future release
-      use_nvim_cmp_as_default = true,
-      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono'
+    -- your own keymap. when defining your own, no keybinds will be assigned automatically.
+    keymap = {
+      preset = "super-tab",
     },
 
-    -- default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, via `opts_extend`
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-      providers = {
-        lsp = {
-          max_items = 25,
-        },
+    appearance = {
+      nerd_font_variant = 'mono',
+    },
+
+    completion = {
+      list = {
+        max_items = 10,
+      },
+    },
+
+    signature = { enabled = true },
+
+    cmdline = {
+      enabled = true,
+      completion = {
+        menu = {
+          auto_show = true,
+        }
       }
     },
 
-    -- experimental auto-brackets support
-    -- completion = { accept = { auto_brackets = { enabled = true } } }
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
 
-    -- experimental signature help support
-    signature = { enabled = true }
+    fuzzy = { implementation = "lua" },
+
   },
-  -- allows extending the providers array elsewhere in your config
-  -- without having to redefine it
-  opts_extend = { "sources.default" }
+  opts_extend = { "sources.default" },
 }
