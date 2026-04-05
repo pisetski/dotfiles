0efc655f6b348cc9da215a4d3ce975b6732cf7ad
pisetski <p.pisetski@gmail.com>
2026-04-05 11:52:24 +0200

    Migrate to nvim 0.12

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,75 +1,53 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  version = "*",
   dependencies = {
     { "windwp/nvim-ts-autotag" },
-    { "nvim-treesitter/nvim-treesitter-refactor" },
   },
-  config = function()
-    require('nvim-treesitter.configs').setup({
-      -- A list of parser names, or 'all'
-      ensure_installed = {
-        'css',
-        'lua',
-        'dockerfile',
-        'dot',
-        'html',
-        'javascript',
-        'json',
-        'make',
-        'markdown',
-        'regex',
-        'tsx',
-        'typescript',
-        'yaml',
-        'gitignore',
-        'bash',
-        'go',
-        'php',
-      },
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = true,
-      -- List of parsers to ignore installing (for 'all')
-      ignore_install = {},
-      highlight = {
-        -- `false` will disable the whole extension
-        enable = true,
-        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-        -- the name of the parser)
-        -- list of language that will be disabled
-        disable = {},
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = false
-      },
-      refactor = {
-        highlight_definitions = {
-          enable = true,
-          -- Set to false if you have an `updatetime` of ~100.
-          clear_on_cursor_move = true,
-        },
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<C-n>',
-          node_incremental = '<C-j>',
-          scope_incremental = false,
-          node_decremental = '<C-k>',
-        },
-      },
-    })
+  lazy = false,
+  branch = "main",
+  init = function()
+    local ensureInstalled = {
+      'lua',
+      'typescript',
+      'css',
+      'dockerfile',
+      'dot',
+      'html',
+      'javascript',
+      'make',
+      'markdown',
+      'regex',
+      'tsx',
+      'yaml',
+      'gitignore',
+      'bash',
+      'go',
+      'php'
+    }
 
+    local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+    local parsersToInstall = vim.iter(ensureInstalled)
+        :filter(function(parser)
+          return not vim.tbl_contains(alreadyInstalled, parser)
+        end)
+        :totable()
+    require('nvim-treesitter').install(parsersToInstall)
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = ensureInstalled,
+      callback = function()
+        -- Enable treesitter highlighting and disable regex syntax
+        vim.treesitter.start()
+        -- Enable treesitter-based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        -- Treesitter-based folding
+        vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        vim.wo[0][0].foldmethod = 'expr'
+      end,
+    })
+  end,
+  config = function()
     require('nvim-ts-autotag').setup({
       opts = {
         enable_close = true,
